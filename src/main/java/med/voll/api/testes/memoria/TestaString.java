3c17ba805b470a33a2712d49b5700818cf7712e8
iasminaraujoc <iasmincorreaaraujo@gmail.com>
2024-05-03 10:30:15 -0300

    Vídeo 2.2

--- src/main/java/med/voll/api/testes/memoria/TestaString.java
+++ src/main/java/med/voll/api/testes/memoria/TestaString.java
@@ -3,10 +3,19 @@ package med.voll.api.testes.memoria;
 public class TestaString {
     public static void main(String[] args) {
         String valor1 = "alura";
-        String valor2 = "alura";
-        System.out.println(valor1 == valor2);
+//        String valor2 = "alura";
+//        System.out.println(valor1 == valor2);
+//
+//        String valor3 = new String("alura");
+//        System.out.println(valor1 == valor3);
 
-        String valor3 = new String("alura");
-        System.out.println(valor1 == valor3);
+        valor1 = valor1 + "Programacao";
+        System.out.println(valor1);
+
+        StringBuilder resultado = new StringBuilder();
+        for(int i = 1; i<=100; i++){
+            resultado.append(i).append(" ");
+        }
+        System.out.println(resultado);
     }
 }
