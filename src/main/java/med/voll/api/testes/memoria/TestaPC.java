e8c6fbac1ae6651db912cdca708af9bed58a299f
iasminaraujoc <iasmincorreaaraujo@gmail.com>
2024-05-07 11:45:40 -0300

    Vídeo 4.2

---
+++ src/main/java/med/voll/api/testes/memoria/TestaPC.java
@@ -0,0 +1,12 @@
+package med.voll.api.testes.memoria;
+
+public class TestaPC {
+    public static void main(String[] args) {
+        System.out.println(soma(3, 5));
+        System.out.println(soma(4, 9));
+    }
+
+    public native static int soma(int valor1, int valor2){
+        return valor1 + valor2;
+    }
+}
